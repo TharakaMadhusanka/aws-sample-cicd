3b26a2386a0e696e5e06dfa3a028d21877df0424
Tharaka Madhusanka <tharakalearn@gmail.com>
2022-05-10 20:33:17 +0530

    Added Dynamic Environment File Generator Step

---
+++ aws-sample/environment-generate.js
@@ -0,0 +1,20 @@
+// This is Environment File Generator
+// This uses Environent Variable in AWS CI/CD to generate Content
+// 2022-05-10
+// Tharaka Madhusanka
+
+function generateEnvironmentContent() {
+  return `export const environment = {
+    production: ${process.env.IS_PRODUCTION || false},
+    environment: ${process.env.PARAM_ENVIRONMENT || 'local' },
+    sampleText: ${process.env.SAMPLE_PARAM_STORE || 'I am from Dynamic Environment :D'}
+  };`
+}
+
+(function generateEnvironment() {
+  const fs = require('node:fs');
+  const fileName = 'environment.ts';
+  const content = generateEnvironmentContent();
+  process.chdir(`src\\environments`);
+  fs.writeFile(fileName, content, () => { console.log('env is generated!') });
+})();
